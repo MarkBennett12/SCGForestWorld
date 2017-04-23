45c25d0a5f75e12021515921793050025ef7c2e4
MarkBennett12 <markusb51@hotmail.com>
2017-04-23 13:27:35 +0100

    First commit of Forest World semantic content generation demo project

---
+++ SCGForestWorld/src/NetworkDefinition.py
@@ -0,0 +1,17 @@
+'''
+Created on 11 Aug 2014
+
+@author: mark
+'''
+
+# This is where the actual semantic network gets created and attributes assigned to nodes and relations.
+
+import SemanticNetwork
+import GameObject
+
+def GetNetwork(globalEnvironment):
+    network = SemanticNetwork.Network(globalEnvironment)
+
+    # Build network here
+        
+    return network
